436d5a587d1398d3720bf8850891d241e6ab4a86
mouad <mouadesoubai123@gmail.com>
2023-08-15 18:13:45 -0700

    test

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -9,22 +9,23 @@
  */
 int print_sign(int n)
 {
-    if (n > 0)
-    {
-        _putchar('+');
-        return (1);
-    } 
-    else if (n == 0)
-    {
-        _putchar('0');
-        return (0);
-    } 
-    else if (n < 0)
-    {
-        _putchar('-');
-        return (-1);
-    }
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+	else if (n < 0)
+	{
+		_putchar('-');
+		return (-1);
+	}
 
-    return 0;
+	/* Default case: return 0 for unsupported values of n */
+	return (0);
 }
 
